aa982c9d8a507826957f5051a8ead824a21fc3aa
mehdi <webtnt.ir@gmail.com>
2026-08-14 12:35:14 +0430

    chore(client): configure tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <h1 className="bg-blue-800 text-white text-2xl font-bold w-full h-screen text-center p-4">
+      hello world
+    </h1>
+  );
 }
 
 export default App;
