264321d3f70f21ae91ddb582663bc22b69a45aba
mehdi <webtnt.ir@gmail.com>
2026-08-31 00:50:00 +0430

    refactor(theme): refactor theme class management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,19 @@ import Content from "./Components/Layout/Content/Content.js";
 import Header from "./Components/Layout/Header/Header.js";
 import Sidebar from "./Components/Layout/Sidebar/Sidebar.js";
 import { useAppSelector } from "./store/hooks.js";
+import { useEffect } from "react";
 
 function App() {
   const { themeMode } = useAppSelector((state) => state.uiManagement);
+  useEffect(() => {
+    if (themeMode == "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [themeMode]);
   return (
-    <main className={themeMode}>
+    <main>
       <div className="dark:text-white">
         <Content />
         <Header />
